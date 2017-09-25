5c376da964e93b3a645bed9cbbfc48e87ef31e44
Unknown <Test0life@gmail.com>
2017-09-24 21:50:22 -0600

    Implemented Character Animations and fixed a couple of bugs.

---
+++ BearPrototypes/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour {
+	public Animator animator;
+	public GameObject character; 
+
+
+	void Update () {
+		if (Input.GetKey (KeyCode.G ))
+			CheckHasObject ();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			animator.SetBool ("jump", true);
+		}else{
+				animator.SetBool ("jump", false);
+			}
+		if (character.GetComponent<CharacterController> ().isGrounded) {
+			animator.SetBool ("grounded", true);
+		}else{
+			animator.SetBool ("grounded", false);
+		}
+		if (character.GetComponent<Move> ().moveDirection.x == 0) {
+			animator.SetBool ("moving", false);
+		} else {
+			animator.SetBool ("moving", true);
+		}
+			
+	}
+	void CheckHasObject(){
+		if (character.GetComponent<Move> ().hasObject == true) {
+			animator.SetBool ("hasObject", true);
+		} else {
+			animator.SetBool ("hasObject", false);
+		}
+	}
+}
---
+++ BearPrototypes/Assets/Scripts/ChickenAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAnimation : MonoBehaviour {
+	public Animator animator;
+	public GameObject chicken;
+
+	void Update(){
+		if (chicken.GetComponent<GrabItem> ().holdingItem == true) {
+			animator.SetBool ("isGrabbed", true);
+		} else {
+			animator.SetBool ("isGrabbed", false);
+		}
+	}
+}
