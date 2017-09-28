bd28ee5365b545893836c89a8201adf3a8daa04b
Unknown <jodamars@gmail.com>
2017-09-28 12:14:06 -0600

    Added Statue sprite and more bug fixing.

--- BearPrototypes/Assets/Scripts/ChangeCheckPoint.cs
+++ BearPrototypes/Assets/Scripts/ChangeCheckPoint.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ChangeCheckPoint : MonoBehaviour {
 	public GameObject checkpoint;
+
 	void OnTriggerEnter(Collider col){
-		if (col.CompareTag ("Player")) {
-			checkpoint.transform.position = transform.position;
+		if (col.CompareTag ("CheckPoint")) {
+			checkpoint.transform.position = col.transform.position;
 			//gameObject.SetActive (false);
 		}
+
 	}
 }
