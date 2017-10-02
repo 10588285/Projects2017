82a1b4de767e4c96b7ff58b85f1b760fd3467add
Unknown <Test0life@gmail.com>
2017-10-01 18:59:53 -0600

    Improved bear follow script and added blinking when character is hurt

--- BearPrototypes/Assets/Scripts/BearFollow.cs
+++ BearPrototypes/Assets/Scripts/BearFollow.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BearFollow : MonoBehaviour {
+	public float speed = 5.0F;
 	public GameObject bear;
 	public Transform defaultPosition;
+	private bool inRange = false; 
+	private GameObject player;
+	void Update(){
+		if (inRange == true) {
+			FollowPlayer ();
+		} else {
+			GoHome ();
+		}
+		if (player &! player.active) {
+			GoHome ();
+		}
+	}
 
 	void OnTriggerEnter(Collider col){
 		if (col.CompareTag("Player") ||col.CompareTag("Chicken")) {
-			StartCoroutine (FollowPlayer (col));
+			player = col.transform.gameObject;
+			inRange = true;
+
 		}
 	}
 	void OnTriggerExit(Collider col){
 		if (col.CompareTag("Player") ||col.CompareTag("Chicken")) {
-			StopCoroutine (FollowPlayer (col));
-			StartCoroutine (GoHome ());
+			inRange = false;
+			player = null;
 		}
 	}
 
-	IEnumerator FollowPlayer(Collider col){
+	void FollowPlayer(){
+		float xPose = Mathf.Lerp (bear.transform.position.x, player.transform.position.x, Time.deltaTime);
+		bear.transform.position = new Vector3 (xPose, defaultPosition.position.y, 0);
+		//bear.transform.position += (player.transform.right * speed * Time.deltaTime);
 
-		float time = 0;
-
-		while (time < 5) {
-			Vector3 target = new Vector3(col.transform.position.x ,bear.transform.position.y,bear.transform.position.z);
-			bear.transform.position = Vector3.MoveTowards (bear.transform.position, target, time);
-			time += Time.deltaTime;
-			yield return null;
-		}
 	}
-	IEnumerator GoHome(){
 
-		float time = 0;
-
-		while (time < 5) {
-			bear.transform.position = Vector3.Lerp (bear.transform.position, defaultPosition.position, time);
-			time += Time.deltaTime;
-			yield return null;
+	void GoHome(){
+		if (bear.transform.position.x != defaultPosition.position.x) {
+			float xPose = Mathf.Lerp (bear.transform.position.x, defaultPosition.position.x, Time.deltaTime);
+			bear.transform.position = new Vector3 (xPose, defaultPosition.position.y, 0);
 		}
 	}
 }
+//myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
--- BearPrototypes/Assets/Scripts/SpriteFlip.cs
+++ BearPrototypes/Assets/Scripts/SpriteFlip.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteFlip : MonoBehaviour {
+	public GameObject sprite;
 	void Update(){
 		Rigidbody body = gameObject.GetComponent<Rigidbody> ();
 		float velocity = body.velocity.x;
 			if (velocity < 0){
-				transform.localRotation = Quaternion.Euler(0, 180, 0);
+				sprite.transform.localRotation = Quaternion.Euler(0, 180, 0);
 			}
 			if(velocity > 0){
-				transform.localRotation = Quaternion.Euler(0, 0, 0);
+				sprite.transform.localRotation = Quaternion.Euler(0, 0, 0);
 			}
 		}
 }
