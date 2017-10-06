8d7ba7e076614be9c6f173a50224fe2ad3ba0cb0
Unknown <Test0life@gmail.com>
2017-10-05 19:57:37 -0600

    more tweaks on lighting scripts

--- BearPrototypes/Assets/Scripts/ChangeSceneColor.cs
+++ BearPrototypes/Assets/Scripts/ChangeSceneColor.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class ChangeSceneColor : MonoBehaviour {
 	public Light lit; 
 	public Material sky;
+	private Color startLight;
+	public Color endLight;
+	private Color startAmbience;
+	public Color endAmbience;
 	private Color startColor;
 	public Color endColor;
 	public float totTime = 5.0F;
 	private float elapTime;
 	void OnTriggerEnter(Collider other){
 		if (other.CompareTag("Player")){
-			StartCoroutine (ChangeColor ());
+				StartCoroutine (ChangeColor ());
+
 		}
 	}
 	void OnTriggerExit(Collider other){
@@ -20,12 +25,19 @@ public class ChangeSceneColor : MonoBehaviour {
 		}
 	}
 	IEnumerator ChangeColor(){
-		print ("entering new zone");
 		startColor = sky.GetColor("_EmissionColor");
+		startAmbience = RenderSettings.ambientLight; 
 
 		while (elapTime < totTime) {
-			print("changing color");
 			elapTime += Time.deltaTime;
+			//change ambient color
+			if (lit) {
+				Color curLight =  Color.Lerp (startLight, endLight, (elapTime/ totTime));
+				lit.color = curLight;
+			}
+			Color curAmbience =  Color.Lerp (startAmbience, endAmbience, (elapTime/ totTime));
+			RenderSettings.ambientLight = curAmbience;
+			//change the sky color
 			Color curColor = Color.Lerp (startColor, endColor, (elapTime/ totTime));
 			sky.SetColor ("_EmissionColor", curColor);
 			yield return null;
