495b337ffbca1c78ed3f089a2e8147ff70d5c09f
Unknown <Test0life@gmail.com>
2017-10-23 21:04:24 -0600

    Added Pause Menu

---
+++ BearPrototypes/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform canvas; 
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+				player.GetComponent<Move> ().enabled = false;
+			} else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+				player.GetComponent<Move> ().enabled = true;
+			}
+		}
+	}
+}
