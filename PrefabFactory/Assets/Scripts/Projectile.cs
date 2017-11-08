b5144ed934a9fe632ad9fed7245ff57f6fa96be3
Unknown <jodamars@gmail.com>
2017-11-08 16:49:25 -0700

    Added Prefab Factory, because I don't want to keep backing it up on the flashdrive

---
+++ PrefabFactory/Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class Projectile : MonoBehaviour {
+
+	private GameObject deathParticle; 
+	void Start () {
+		deathParticle = GameObject.Find ("Particle_Exlosion");
+		//StartCoroutine (Recycle());
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("destructible")) {
+			Debug.Log ("Hit");
+			NetworkServer.Spawn (deathParticle);
+			//gameObject.SetActive (false);
+		}
+	}
+	void OnDisable(){
+		
+	}
+	/*
+	IEnumerator Recycle(){
+		 return null; 
+	}
+	*/
+}
