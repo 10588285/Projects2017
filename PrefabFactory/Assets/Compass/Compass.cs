708d312cdb3b81ed068f8b7a74c26e76efb51376
Unknown <jodamars@gmail.com>
2017-12-06 11:04:01 -0700

    Wooden Plank textures, and started temple textures.

---
+++ PrefabFactory/Assets/Compass/Compass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+	//transform values of the tank and turret
+	public Transform tank;
+	public Transform turret;
+	public Transform nwse;
+	//Spindle objects 
+	public Transform tankSpind;
+	public Transform turretSpind;
+	public Transform compassSpind; 
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		//assign the rotation values of the tank objects to the spindles. 
+		tankSpind.rotation = tank.rotation;
+		turretSpind.rotation = turret.rotation;
+		compassSpind.rotation = nwse.rotation;
+	}
+}
