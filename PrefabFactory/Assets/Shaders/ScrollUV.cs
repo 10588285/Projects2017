3c968344084a2a0b58fd7c5216de490700faf4a1
Unknown <jodamars@gmail.com>
2018-01-11 12:43:05 -0700

    Started Binoculars for style guide test

--- PrefabFactory/Assets/Shaders/ScrollUV.cs
+++ PrefabFactory/Assets/Shaders/ScrollUV.cs
@@ -6,11 +6,14 @@ public class ScrollUV : MonoBehaviour {
 
 	public Vector2 scrollSpeed;
 	private Renderer texture;
-
+	public string texName = "_MainTex";
+	private Vector2 offset;
 	void Start(){
 		texture = GetComponent<Renderer> ();
 	}
 	void Update () {
-		texture.material.SetTextureOffset  (scrollSpeed.x,scrollSpeed.y);
+		offset += scrollSpeed * Time.deltaTime;
+		texture.material.SetTextureOffset (texName, offset);
+
 	}
 }
