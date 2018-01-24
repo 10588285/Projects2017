51e123f5cf240a9b019b095b5758a893fb966fbf
Unknown <Test0life@gmail.com>
2018-01-24 07:44:50 -0700

    Auto stash before merge of "develop" and "origin/develop"
    merging everything

--- BearPrototypes/Assets/Scripts/ResetObjects.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ResetObjects : MonoBehaviour {
-	public List<GameObject> resetObjects;
-	public AudioSource music;
-	public float musicVolume;
-}
