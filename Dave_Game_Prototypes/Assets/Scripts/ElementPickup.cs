523f38c33d851e6a4b11ae77b0e4669681b314e6
Unknown <jodamars@gmail.com>
2018-01-25 15:06:35 -0700

    Started Collection prototype

---
+++ Dave_Game_Prototypes/Assets/Scripts/ElementPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ElementPickup : MonoBehaviour {
+	//Text UI that display the name of the element the play currently has. 
+	private Text elementTxt;
+	void Start(){
+		//elementTxt = GameObject.Find ("ElementDisplayText").GetComponent<Text>();
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Animal")){
+			other.gameObject.SetActive (false);
+			print(other.gameObject.GetComponent<Animal> ().type);
+			GetComponent<Player>().currentElement = other.gameObject.GetComponent<Animal> ().type;
+		}
+	}
+}
---
+++ Dave_Game_Prototypes/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	//class to manager the states of the player
+
+	public Elemental.elementType currentElement;
+	void Start()
+	{
+
+		currentElement = Elemental.elementType.Neutral;
+
+	}
+
+}
