1a60e6a7285e8e7dae1a9df743a2bec1dd3648e9
Unknown <Test0life@gmail.com>
2018-01-24 22:40:04 -0700

    whitebox for environment

---
+++ WhiteBox_Temp/Assets/Scripts/CopyXRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Copys the y rotation of an object and apllies it to the object with this script attached to it. 
+
+public class CopyXRotation : MonoBehaviour {
+	
+	public GameObject copyObject;
+
+	void LateUpdate(){
+		Vector3 newRotation = new Vector3 (copyObject.transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
+		transform.eulerAngles = newRotation;
+	}
+}
