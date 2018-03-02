20acdb6bebeff3c017ab0bbadcc31e13e9136351
Unknown <Test0life@gmail.com>
2018-03-02 07:11:02 -0700

    Continued work on tank interior and environment

--- Dave_Game_Prototypes/Assets/Scripts/Abs_Test.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Abs_Test: ScriptableObject{
-
-	public abstract void TestMethod (string newString);
-
-}
