84b0f7b4a0d9fe2cfb9f5ec9b1db62e72930df4e
Unknown <Test0life@gmail.com>
2018-03-04 22:43:08 -0700

    added animation test project and finished organizing the tank scene.

---
+++ Animation_Test/Assets/Scripts/Animation_Control_Character.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Control_Character : MonoBehaviour {
+
+	public string parameter;
+	private bool lookingBack = false;
+	private Animator animator;
+	public AudioSource switchSound;
+
+	void Start(){
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			ToggleState ();
+		}
+	}
+
+	void ToggleState(){
+		if (lookingBack == true) {
+			lookingBack = false;
+			animator.SetBool (parameter, false);
+		} else {
+			lookingBack = true;
+			animator.SetBool (parameter, true);
+		}
+		switchSound.Play ();
+	}
+}
---
+++ Animation_Test/Assets/Scripts/StepMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepMotion : MonoBehaviour {
+
+	public Transform stepTarget;
+	public float animationSpeed;
+	// Update is called once per frame
+	void Start(){
+		animationSpeed = 1 / animationSpeed;
+		InvokeRepeating ("Step",0,animationSpeed);
+	}
+
+	void Step(){
+		transform.position = stepTarget.position;
+
+	}
+}
