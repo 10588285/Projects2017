2ed153b7bd8a0e4885e33ef032635b9ea41d3186
Unknown <Test0life@gmail.com>
2018-03-26 23:02:47 -0600

    Started Game scene, and moved 002 and prefab projects

---
+++ Furnace_Environment/Furnace_Env/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public float xMax = 20.0f;
+	public float xMin = -20.0f;
+	public float yMax = 20.0f;
+	public float yMin = -20.0f;
+	public Transform charPose;
+	private Vector3 target; 
+
+	void Update(){
+		target.x = Mathf.Lerp (transform.position.x, charPose.position.x, Time.deltaTime * speed);
+		target.y = Mathf.Lerp (transform.position.y, charPose.position.y, Time.deltaTime * speed);
+		target.x = Mathf.Clamp (target.x, xMin, xMax);
+		target.y = Mathf.Clamp (target.y, yMin, yMax);
+		transform.position = target;
+
+	}
+
+}
---
+++ Furnace_Environment/Furnace_Env/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 15.0f;
+	public CharacterController cc;
+	public Vector3 moveDir;
+	public int jumpCount = 2;
+	private int jump;
+	void Start(){
+		jump = jumpCount;
+		cc = GetComponent<CharacterController> ();
+	}
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+		moveDir.x = -(Input.GetAxis ("Horizontal") * speed);
+		if (cc.isGrounded) {
+			jumpCount = jump;
+		} else {
+			moveDir.y -= gravity * Time.deltaTime;
+		}
+		if(Input.GetKeyDown (KeyCode.W) && jumpCount > 0) {
+			moveDir.y = jumpSpeed;
+			jumpCount--;
+		}
+		cc.Move(moveDir * Time.deltaTime);
+	}
+}
