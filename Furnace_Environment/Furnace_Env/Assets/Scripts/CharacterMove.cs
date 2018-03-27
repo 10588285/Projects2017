aa6527a081b819205bded9722fbbe561db0f1696
Unknown <jodamars@gmail.com>
2018-03-27 14:44:59 -0600

    added ladder climbing function.

--- Furnace_Environment/Furnace_Env/Assets/Scripts/CharacterMove.cs
+++ Furnace_Environment/Furnace_Env/Assets/Scripts/CharacterMove.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	enum moveState {move, climb};
+	moveState state = moveState.move;
 	public float speed = 5.0f;
+	public float climbSpeed = 2.0f;
 	public float jumpSpeed = 8.0f;
 	public float gravity = 15.0f;
 	public CharacterController cc;
@@ -12,16 +15,29 @@ public class CharacterMove : MonoBehaviour {
 	public int jumpCount = 2;
 	private int jump;
 	void Start(){
+		state = moveState.move;
 		jump = jumpCount;
 		cc = GetComponent<CharacterController> ();
 	}
 	void Update () {
-		Move ();
+
+		switch (state) {
+		case moveState.climb:
+			Climb ();
+			break;
+		case moveState.move:
+			Move ();
+			break;
+		}
+
 	}
 
 	void Move(){
-		moveDir.x = -(Input.GetAxis ("Horizontal") * speed);
+		//sideways movement
+		SideMove ();
+		//jumping and gravity
 		if (cc.isGrounded) {
+			//when grounded the jump count resets
 			jumpCount = jump;
 		} else {
 			moveDir.y -= gravity * Time.deltaTime;
@@ -31,5 +47,26 @@ public class CharacterMove : MonoBehaviour {
 			jumpCount--;
 		}
 		cc.Move(moveDir * Time.deltaTime);
+	}
+
+	void Climb(){
+		SideMove ();
+		moveDir.y = (Input.GetAxis ("Vertical") * climbSpeed);
+		cc.Move(moveDir * Time.deltaTime);
+	}
+
+	void SideMove(){
+		moveDir.x = -(Input.GetAxis ("Horizontal") * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("Ladder")) {
+			state = moveState.climb;
+		}
+	}
+	void OnTriggerExit(Collider other){
+		if (other.CompareTag ("Ladder")) {
+			state = moveState.move;
+		}
 	}
 }
---
+++ Furnace_Environment/Furnace_Env/Assets/Scripts/StepMotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepMotion : MonoBehaviour {
+
+	public  Transform target;
+	public float stepSpeed = 12;
+	void Start(){
+		Step ();
+	}
+
+	void Step(){
+		transform.position = target.position;
+		Invoke ("Step", (1/stepSpeed));
+	}
+}
