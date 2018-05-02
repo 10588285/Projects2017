31b5961ec27129dd83e7505c456433329e680ae1
Unknown <Test0life@gmail.com>
2018-05-01 21:41:15 -0600

    Put a hole in the ground

---
+++ WhiteBox_Temp/Assets/CreateRubble.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRubble : MonoBehaviour {
+
+	public GameObject ShatteredWall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "projectile")
+		{
+			ShatteredWall.SetActive (true);
+			this.gameObject.SetActive (false);
+			col.gameObject.SetActive (false);
+
+		}
+		if (col.gameObject.tag == "Explosion")
+		{
+			ShatteredWall.SetActive (true);
+			this.gameObject.SetActive (false);
+		}
+		print (col.gameObject.name);
+		
+			
+	}
+}
